e1748a6e1a00d97172a08a9ed47f9110b1336a44
Chintan Nyalvani <chintanrokcj10@gmail.com>
2022-07-10 13:23:58 -0700

    'SohniJunejaClone'

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@include("header");
+
+<center><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13767.780286907804!2d78.0860112!3d30.3809176!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x13e8933f0d1cb787!2sMakeup%20by%20Sohni%20Juneja!5e0!3m2!1sen!2sin!4v1657281138529!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></center>
+
+
+@include("footer");
